a7af4fa4b46771a40c79feee4441bc34bba17447
LAPTOP-J86BFAT7\中野ゆうま <n.yuma0901@gmail.com>
2020-12-03 22:27:56 +0900

    キャラクター表示

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, caption="Hello Pyxel")
+        pyxel.load("sample.pyxres")
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -12,6 +13,9 @@ class App:
     def draw(self):
         pyxel.cls(5)
         pyxel.text(55, 41, "Hello World!", pyxel.frame_count % 16)
+        # draw player
+        pyxel.blt(10, 10, 0, 0, 0, 16, 16, 12)
+
 
 
 App()
